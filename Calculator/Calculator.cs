14a1d28ce5c3843226f888398117d65b9a086442
iradukundapaci <paifique@gmail.com>
2023-09-12 09:15:08 +0200

    adding calcutor project

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,22 @@
+namespace CalculatorNamespace
+{
+    public class Calculator
+    {
+        public float addition(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+        public float subtraction(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+        public float division(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+        public float multiplication(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using CalculatorNamespace;
+
+Calculator calculator = new Calculator();
+
+while (true)
+{
+    Console.Write("Enter operation number:\n 1 Addition\n 2 Subtraction\n 3 Multiplication\n 4 Division\n 0 EXIT\n Choice: ");
+    int choice = Convert.ToInt32(Console.ReadLine());
+    if (choice <= 0)
+    {
+        Console.WriteLine("Exiting program");
+        break;
+    }
+    else if (choice > 4)
+    {
+        Console.WriteLine("Unkown Choice \n");
+        continue;
+    }
+    Console.Write("Enter first operand: ");
+    float firstOperand = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter second operand: ");
+    float secondOperand = Convert.ToInt32(Console.ReadLine());
+
+
+    switch (choice)
+    {
+        case 1:
+            Console.Write("\nThe result is " + calculator.addition(firstOperand, secondOperand) + "\n\n");
+            break;
+        case 2:
+            Console.Write("\nThe result is " + calculator.subtraction(firstOperand, secondOperand) + "\n\n");
+            break;
+        case 3:
+            Console.Write("\nThe result is " + calculator.multiplication(firstOperand, secondOperand) + "\n\n");
+            break;
+        case 4:
+            if (secondOperand == 0)
+            {
+                Console.WriteLine("Division by zero is impossible");
+                break;
+            }
+            Console.Write("\nThe result is " + calculator.division(firstOperand, secondOperand) + "\n\n");
+            break;
+    }
+}
